52421c8f4266375fb3550e16f0c77bc0cb4e673d
Cool BRG <budhram.gurung01@gmail.com>
2020-03-28 09:00:31 +0530

    Finalized the banner area
    * added style to brand text logo
    * added color pallete
    * added youtube video for understanding corona

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,4 +2,22 @@
 $(function(){
   // Activate wow.js
   new WOW().init();
+
+  // rendering youtube video in the Modal
+  var $videoSrc;
+  $(".youtube-video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+  // when the modal is opened autoplay it
+  $("#youtubeVideoModal").on("shown.bs.modal", function (e) {
+    $("#youtubeVideo").attr("src", $videoSrc);
+  });
+
+  // stop playing the youtube video when I close the modal
+  $("#youtubeVideoModal").on("hide.bs.modal", function (e) {
+    $("#youtubeVideo").replaceWith(
+      '<iframe class="embed-responsive-item" src="" id="youtubeVideo" allowscriptaccess="always" allow="autoplay"></iframe>'
+    );
+  });
 });
