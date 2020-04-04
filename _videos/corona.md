6fb8498ee14eb9e2a864d0ba377a5cbfdf75a2ab
Cool BRG <budhram.gurung01@gmail.com>
2020-04-04 15:21:14 +0530

    Fix #10 Embed facebook video and added one for videos

--- _videos/corona.md
+++ _videos/corona.md
@@ -7,6 +7,12 @@ list:
     source: Poor Mans Studio
     source_url: https://www.youtube.com/channel/UCs-uGNRHAn_yJB3oFvjzM7w
   - title: "साबुन पानीले मिचिमिचि हात धोऔँ, रोगबाट बचौँ"
+    src: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fwatch%2F%3Fv%3D306908343613872&width=500&show_text=false&height=280&appId"
+    thumbnail: https://scontent.fnag4-1.fna.fbcdn.net/v/t15.5256-10/91821234_306928860278487_2841896606117134336_n.jpg?_nc_cat=100&_nc_sid=f2c4d5&_nc_ohc=UCN2Cw0c5skAX_83nyk&_nc_ht=scontent.fnag4-1.fna&oh=ff0042cc8135277f983c5de63da15aa4&oe=5EAED45F
+    source_platform: facebook
+    source: HAPSA
+    source_url: https://www.facebook.com/hapsa4all/
+  - title: "साबुन पानीले मिचिमिचि हात धोऔँ, रोगबाट बचौँ"
     id: Oi1RmKGPbF0
     source: UNICEF Nepal
     source_url: https://www.youtube.com/channel/UCOvOOXl60cMNAxwdjWN1j0w
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 /*Custom JavaScript*/
 // Lazy Loading of images
 document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages;    
+  var lazyloadImages;
 
   if ("IntersectionObserver" in window) {
     lazyloadImages = document.querySelectorAll(".lazy");
@@ -19,14 +19,14 @@ document.addEventListener("DOMContentLoaded", function() {
     lazyloadImages.forEach(function(image) {
       imageObserver.observe(image);
     });
-  } else {  
+  } else {
     var lazyloadThrottleTimeout;
     lazyloadImages = document.querySelectorAll(".lazy");
-    
+
     function lazyload () {
       if(lazyloadThrottleTimeout) {
         clearTimeout(lazyloadThrottleTimeout);
-      }    
+      }
 
       lazyloadThrottleTimeout = setTimeout(function() {
         var scrollTop = window.pageYOffset;
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
               img.classList.remove('lazy');
             }
         });
-        if(lazyloadImages.length == 0) { 
+        if(lazyloadImages.length == 0) {
           document.removeEventListener("scroll", lazyload);
           window.removeEventListener("resize", lazyload);
           window.removeEventListener("orientationChange", lazyload);
@@ -69,6 +69,26 @@ $(function(){
   $("#youtubeVideoModal").on("hide.bs.modal", function (e) {
     $("#youtubeVideo").replaceWith(
       '<iframe class="embed-responsive-item" src="" id="youtubeVideo" allowscriptaccess="always" allow="autoplay"></iframe>'
+    );
+  });
+
+  // ------------ rendering facebook video in the modal
+  var $videoSrc;
+  $(".facebook-video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+  // when the modal is opened autoplay it
+  $("#facebookVideoModal").on("shown.bs.modal", function (e) {
+    $("#facebookVideo").attr("src", $videoSrc);
+  });
+
+  // stop playing the facebook video when I close the modal
+  $("#facebookVideoModal").on("hide.bs.modal", function (e) {
+    $("#facebookVideo").replaceWith(
+      '<iframe id="facebookVideo" class="embed-responsive-item" src="" style="border:none;overflow:hidden"\
+          scrolling = "no" frameborder = "0" allowTransparency = "true" allow = "encrypted-media"\
+          allowFullScreen = "true" ></iframe>'
     );
   });
 });
